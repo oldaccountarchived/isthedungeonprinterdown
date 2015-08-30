c0603ce17e6b88e997d27301294090b43726e97e
Jacob Jenkins <jacob@jacobejenkins.com>
2015-08-29 21:35:04 -0400

    Here's the start of a controller.

---
+++ controller.js
@@ -0,0 +1,41 @@
+angular
+    .module('core', [])
+    .controller('StatusController', ['$scope'], StatusController);
+
+function StatusController($scope) {
+
+    // Shamelessly ripped from stack overflow because that's the
+    // spirit of weekend hacking and this isn't node.js so I
+    // don't have that swanky crypto module.
+    function randomString(length, chars) {
+        var result = '';
+        for (var i = length; i > 0; --i) {
+            result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+        return result;
+    }
+    
+    $scope.localAuth = function() {
+        var keylength = 128;
+        var characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (!localStorage.key || localStorage.key.length < keylength) {
+            // Yup, you could totally just set this thing to something
+            // else and fuck my day up.
+            localStorage.key = randomString(
+                keylength, characters
+            );
+        }
+    };
+
+    $scope.getStatus = function() {
+        
+    };
+    
+    $scope.voteDown = function() {
+        
+    };
+
+    $scope.voteUp = function() {
+
+    };
+};
